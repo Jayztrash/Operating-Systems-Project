f00d342e08bd6c2049c5981bdcb7ea585fc7f758
Krishna Amuluru <krishna.amuluru@gmail.com>
2025-03-17 15:28:30 -0400

    temp commit with a few errors

--- Process.java
+++ Process.java
@@ -1,15 +1,61 @@
+public enum PCBState {
+    NEW,
+    READY,
+    RUNNING,
+    BLOCKED,
+    SUSPENDED,
+    SUSPENDED_BLOCKED,
+    TERMINATED
+}
+
 class Process {
     int pid;
+    PCBState state;
     int arrivalTime;
     int burstTime;
     int priority;
-    int waitingTime;
-    int turnaroundTime;
+    double waitingTime;
+    double turnaroundTime;
+    Set<Resource> resources = new HashSet<>();
 
-    public Process(int pid, int arrivalTime, int burstTime, int priority) {
+
+    public Process(int pid, PCBState state, int arrivalTime, int burstTime, int priority, Set<Resources> resources) {
         this.pid = pid;
+        this.state = state;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.priority = priority;
+        this.resources = resources;
     }
+
+    void activate() {
+        state = PCBState.READY;
+    }
+
+    void suspend() {
+        if (this.state == PCBState.READY || this.state == PCBState.RUNNING) {
+            this.state = PCBState.SUSPENDED;
+        } elif (this.state == PCBState.BLOCKED) {
+            this.state = PCBState.SUSPENDED_BLOCKED;
+        }
+        //scheduler();
+    }
+
+    void reactivate() {
+        if (this.state == PCBState.SUSPENDED) {
+            this.state = PCBState.READY
+        } elif (this.state == PCBState.SUSPENDED_BLOCKED) {
+            this.state = PCBState.BLOCKED;
+        }
+    }
+
+    void request(Resource r) {
+        if (this.state == PCBState.READY || this.state == PCBState.RUNNING) {
+            if (r.state = ResourceState.FREE) {
+                r.state = ResourceState.ALLOCATED
+            }
+        }
+    }
+
+
 }
--- Resource.java
+++ Resource.java
@@ -1,3 +1,17 @@
-class Resource {
+import java.util.*
 
+public enum ResourceState {
+    FREE,
+    ALLOCATED
+}
+
+
+class Resource {
+    ResourceState state;
+    LinkedList<Process> waitingList;
+
+    Resource(ResourceState state, LinkedList<Process> waitingList) {
+        this.state = FREE;
+        this.waitingList = waitingList;
+    }
 }
