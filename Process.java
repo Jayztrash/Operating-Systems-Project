5739b4e805459bd3d0d9dd0c6e1c8558b7a0fd30
Jayztrash <jayvion1022@gmail.com>
2025-03-20 16:49:41 -0400

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,14 +1,21 @@
 import java.util.*;
-
+// represents a process in our system
 public class Process {
+    // process id
     int pid;
+    // when the process arrives in the system
     int arrivalTime;
+    // how long the process needs the CPU
     int burstTime;
+    // priority of the process
     int priority;
+    // for rr scheduling
     int remainingTime; 
+    // time spent waiting after running
     int waitingTime;
+    // total time from arrival to completion
     int turnaroundTime;
-
+    // initializes a process object with its ID, arrival time, burst time, and priority
     public Process(int pid, int arrivalTime, int burstTime, int priority) {
         this.pid = pid;
         this.arrivalTime = arrivalTime;
