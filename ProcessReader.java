815866e4a701805aa5906257bc7274bbe995a0f1
Jayztrash <jayvion1022@gmail.com>
2025-03-20 16:42:21 -0400

    Update and rename Process_reader.java to ProcessReader.java

--- ProcessReader.java
+++ ProcessReader.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
-
+// reads process data from file
 public class ProcessReader {
+    // stores the data in a list
     public static List<Process> readProcesses(String filename) {
         List<Process> processes = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(filename))) {
@@ -13,6 +14,7 @@ public class ProcessReader {
                     int arrival = Integer.parseInt(parts[1]);
                     int burst = Integer.parseInt(parts[2]);
                     int priority = Integer.parseInt(parts[3]);
+                   // creates a new process and adds it to the list
                     processes.add(new Process(pid, arrival, burst, priority));
                 }
             }
